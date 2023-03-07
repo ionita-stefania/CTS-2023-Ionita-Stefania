dd5364ae9d70ae30531da57c462f885506cef762
ionita-stefania <ionita.stefaniaa@gmail.com>
2023-03-07 21:12:12 +0200

    main

--- Documents/1.2 ANUL 3 SEM 2/CTS/Tema SOLID/src/ro/ase/acs/cts/interfaces/Reader.java
+++ Documents/1.2 ANUL 3 SEM 2/CTS/Tema SOLID/src/ro/ase/acs/cts/interfaces/Reader.java
@@ -1,4 +1,8 @@
 package ro.ase.acs.cts.interfaces;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public interface Reader {
+    void readData(Connection connection) throws SQLException;
 }
